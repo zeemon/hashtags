a2261b476202409c255e586b611756084588bac7
Naizy <zymonteiro@gmail.com>
2017-08-18 16:37:28 +0100

    read the uploaded files and count the word occurrences

--- app/views.py
+++ app/views.py
@@ -1,16 +1,48 @@
 from django.views import View
-from collections import Counter
 from django.http import HttpResponse
 from django.shortcuts import render
+import re
 
 class HashCounter(View):
 
     def post(self, request, *args, **kwargs):
-        c = Counter()
+        self.hashtags = {}
         files= request.FILES.getlist('hashfile')
+        
         for f in files:
             f.file.seek(0)
-            file_text= f.read()
-            c.update(file_text.split())
+            for line in self.read_lines_of_file(f):
+                # each line can have multiple sentances.
+                # split sentences into an array. also take questions and exclamations into consideration 
+                sentences = re.split('[?!.][\s]*',line.decode('utf-8').lower())
+                for s in sentences:
+                    if s:
+                        words = re.findall(r'\w+', s)
+                        [self.process_word(f, s, word) for word in words]
 
-        return render(request, 'hashtags.html', {'hashtags': c.most_common()})
+        self.hashtags= sorted(self.hashtags.items(), key=lambda x: x[1]['count'], reverse=True)
+        return render(request, 'hashtags.html', {'hashtags': self.hashtags})
+
+    def read_lines_of_file(self, file):
+        while True:
+            data = file.readline()
+            if not data:
+                break
+            yield data    
+
+    def process_word(self, file, sentence, word):
+        if word in self.hashtags:
+            hw = self.hashtags[word]
+            self.hashtags.update({
+                word: {
+                    'count': hw['count'] +1,
+                    'documents': hw['documents'] + [file.name] if not file.name in hw['documents'] else hw['documents'],
+                    'sentences': hw['sentences'] + [sentence] if not sentence in hw['sentences'] else hw['sentences']
+                }
+            }) 
+        else:
+            self.hashtags[word]={
+                'count' : 1,
+                'documents' : [file.name],
+                'sentences' : [sentence] 
+            }        
